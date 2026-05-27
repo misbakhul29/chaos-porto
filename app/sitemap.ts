d87fbee547268634f01f6811dac74922d4750fa2
misbakhul_munir <misbakhul.munir@rakhasa.com>
2026-05-27 16:41:02 +0700

    feat: add about and projects pages to sitemap definition

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "monthly",
             priority: 1,
         },
+        {
+            url: "https://www.misbakhul.com/about",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: "https://www.misbakhul.com/projects",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
     ];
 }
